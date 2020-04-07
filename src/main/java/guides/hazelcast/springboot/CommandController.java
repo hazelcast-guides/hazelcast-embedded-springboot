a3a2a22f9565940317054fc2fbe63dca49cbf3ca
alparslanavci <alparslanavci@gmail.com>
2020-04-07 12:07:28 +0100

    Review updates

--- src/main/java/guides/hazelcast/springboot/CommandController.java
+++ src/main/java/guides/hazelcast/springboot/CommandController.java
@@ -4,7 +4,6 @@ import com.hazelcast.core.HazelcastInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/test/java/guides/hazelcast/springboot/CommandControllerIT.java
+++ src/test/java/guides/hazelcast/springboot/CommandControllerIT.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.junit.Assert.assertEquals;
 import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -26,13 +27,13 @@ public class CommandControllerIT {
     @Test
     public void testPutRequest(){
         //when
-        WebTestClient.ResponseSpec responseSpec = makeRequest("/put?key={key}&value={value}", "key1", "value1");
+        WebTestClient.ResponseSpec responseSpec = makePostRequest("/put?key={key}&value={value}", "key1", "value1");
 
         //then
         responseSpec.expectStatus()
                 .is2xxSuccessful()
                 .expectHeader()
-                .contentType(APPLICATION_JSON_UTF8)
+                .contentType(APPLICATION_JSON)
                 .expectBody()
                 .jsonPath("$.value").isEqualTo("value1");
     }
@@ -40,23 +41,27 @@ public class CommandControllerIT {
     @Test
     public void testGetRequest(){
         //given
-        makeRequest("/put?key={key}&value={value}", "key1", "value1");
+        makePostRequest("/put?key={key}&value={value}", "key1", "value1");
 
         //when
-        WebTestClient.ResponseSpec responseSpec = makeRequest("/get?key={key}", "key1");
+        WebTestClient.ResponseSpec responseSpec = webTestClient
+                .get()
+                .uri("/get?key={key}", "key1")
+                .header(ACCEPT, APPLICATION_JSON_VALUE)
+                .exchange();
 
         //then
         responseSpec.expectStatus()
                 .is2xxSuccessful()
                 .expectHeader()
-                .contentType(APPLICATION_JSON_UTF8)
+                .contentType(APPLICATION_JSON)
                 .expectBody()
                 .jsonPath("$.value").isEqualTo("value1");
     }
 
-    private WebTestClient.ResponseSpec makeRequest(String uri, Object... parameters) {
+    private WebTestClient.ResponseSpec makePostRequest(String uri, Object... parameters) {
         return webTestClient
-                .get()
+                .post()
                 .uri(uri, parameters)
                 .header(ACCEPT, APPLICATION_JSON_VALUE)
                 .exchange();
